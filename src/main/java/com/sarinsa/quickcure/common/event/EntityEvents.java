8131fa914731b121a68ee531654298b7c5f3b752
Sarinsa <mikkemusbruker@gmail.com>
2021-07-24 02:37:20 +0200

    Ported to 1.17.1

--- src/main/java/com/sarinsa/quickcure/common/event/EntityEvents.java
+++ src/main/java/com/sarinsa/quickcure/common/event/EntityEvents.java
@@ -1,7 +1,7 @@
 package com.sarinsa.quickcure.common.event;
 
-import net.minecraft.entity.monster.ZombieVillagerEntity;
-import net.minecraft.world.server.ServerWorld;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.monster.ZombieVillager;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -9,12 +9,13 @@ public class EntityEvents {
 
     @SubscribeEvent
     public void onZombieVillagerCure(LivingEvent.LivingUpdateEvent event) {
-        if (event.getEntityLiving() instanceof ZombieVillagerEntity) {
-            ZombieVillagerEntity zombieVillager = (ZombieVillagerEntity) event.getEntityLiving();
 
-            if (zombieVillager.isAlive() && zombieVillager.isConverting()) {
-                if (!zombieVillager.level.isClientSide) {
-                    zombieVillager.finishConversion((ServerWorld) zombieVillager.level);
+        if (event.getEntityLiving() instanceof ZombieVillager zombieVillager) {
+                             // isAlive                   // isConverting
+            if (zombieVillager.m_6084_() && zombieVillager.m_34408_()) {
+                                           // isClientSide
+                if (!zombieVillager.f_19853_.f_46443_) {
+                    zombieVillager.m_34398_((ServerLevel) zombieVillager.f_19853_);
                 }
             }
         }
